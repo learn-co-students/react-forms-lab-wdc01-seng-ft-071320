a1f35924fc0f69e535173415ee294ea466d8a4a8
Catrina Friday <cat.friday@gmail.com>
2020-09-21 22:37:42 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,40 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
   }
+
+  changeUpdate = (e) =>{
+    let name = e.target.name
+  let value = e.target.value
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+ submit = (e) =>{
+   e.preventDefault()
+   if (!this.state.username || !this.state.password) return
+   this.props.handleLogin(this.state)
+ }
 
   render() {
     return (
-      <form>
+      <form onSubmit={(e) => this.submit(e)}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" value={this.state.username} onChange={(e) => this.changeUpdate(e)}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" value={this.state.password} onChange={(e) => this.changeUpdate(e)}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,28 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ''
+    };
   }
 
+
+changeValue = (value) =>{
+  // let name = e.target.name
+  // let value = e.target.value
+  this.setState({
+    message: value
+  })
+  console.log(value)
+}
+
   render() {
+    let charNumber = this.props.maxChars - this.state.message.length;
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.state.message} onChange={(e) => this.changeValue(e.target.value)}/>
+        {charNumber}
       </div>
     );
   }
